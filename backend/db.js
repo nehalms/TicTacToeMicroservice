ffe4b53a7ca8e891c1d56abec4e0b39025d83e8a
nehalms <116992269+nehalms@users.noreply.github.com>
2024-09-25 12:47:53 +0530

    Delete backend directory

--- backend/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-// const mongoURI = "mongodb://127.0.0.1/inotebook" //inotebook is database name
-const connectToMongo = ()=> {
-    mongoose.connect(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.ifjjiuu.mongodb.net/inotebook?retryWrites=true&w=majority&appName=Cluster0`).then(console.log("Connected to Mongodb"));
-}
-
-module.exports = connectToMongo;
